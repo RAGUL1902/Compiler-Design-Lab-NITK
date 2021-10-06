8daba3f279ed30d02720504d0b908e3d80eadf0d
Ragul1902 <ragulsenthilkumar02@gmail.com>
2021-10-06 11:12:43 +0530

    Formatted week-3/q1.c

--- Week - 3/Q1/q1.c
+++ Week - 3/Q1/q1.c
@@ -1,6 +1,6 @@
 //1. Write a program to find LR(0) items
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 char productions[50][100];
 char LR_0[150][100];
@@ -9,43 +9,43 @@ int main()
 {
     int n;
     printf("Enter number of productions in the grammar: ");
-    scanf("%d",&n);
-    printf("Enter productions in the form A->B (enter A->B|C as 2 separate productions): \n");
-    for(int i=0;i<n;i++)
+    scanf("%d", &n);
+    printf("Enter productions in the form A->B: \n");
+    for (int i = 0; i < n; i++)
     {
-        
-        scanf("%s",productions[i]);
+
+        scanf("%s", productions[i]);
         getchar();
     }
-    memset(LR_0,'!',sizeof(LR_0));
+    memset(LR_0, '!', sizeof(LR_0));
     //computing lr 0 items
-    int lr_index=0;
-    for(int i=0;i<n;i++)
+    int lr_index = 0;
+    for (int i = 0; i < n; i++)
     {
         int prod_len = strlen(productions[i]);
-        for(int j=3;j<=prod_len;j++)
+        for (int j = 3; j <= prod_len; j++)
         {
             int k;
-            for(k=0;k<j;k++)
+            for (k = 0; k < j; k++)
             {
                 LR_0[lr_index][k] = productions[i][k];
             }
             LR_0[lr_index][k] = '.';
-            for(++k;k<=prod_len;k++)
+            for (++k; k <= prod_len; k++)
             {
-                LR_0[lr_index][k] = productions[i][k-1];
+                LR_0[lr_index][k] = productions[i][k - 1];
             }
             LR_0[lr_index][k] = '\0';
             lr_index++;
-            if(productions[i][j] == '#')//in this case only .# is taken and not #. so breaking after getting .#
+            if (productions[i][j] == '#') //in this case only .# is taken and not #. so breaking after getting .#
                 break;
         }
     }
     LR_len = lr_index;
     printf("LR(0) items are: \n");
-    for(int i=0;i<LR_len;i++)
+    for (int i = 0; i < LR_len; i++)
     {
-        printf("\t%s\n",LR_0[i]);
+        printf("\t%s\n", LR_0[i]);
     }
     return 0;
 }
